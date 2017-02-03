8e9b563583ce945b835fba545d5acbe2e11bb763
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2017-02-02 19:11:42 -0600

    rename all files so that I can get syntax highlighting on them, as well as fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -107,9 +107,9 @@ install_dotfiles () {
 
   local overwrite_all=false backup_all=false skip_all=false
 
-  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
+  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink*' -not -path '*.git*')
   do
-    dst="$HOME/.$(basename "${src%.*}")"
+    dst="$HOME/.$(basename "${src%.symlink*}")"
     link_file "$src" "$dst"
   done
 }
