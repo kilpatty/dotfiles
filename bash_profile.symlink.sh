dde1b745b5d9c740d582b5ed2797cbebb7a1fca6
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2017-02-28 17:07:14 -0600

    add bash completion back into bash profile

--- bash_profile.symlink.sh
+++ bash_profile.symlink.sh
@@ -22,6 +22,11 @@ for option in autocd globstar; do
 	shopt -s "$option" 2> /dev/null;
 done;
 
+# Enable Bash Completion if it exists
+if [ -f "$(brew --prefix)"/share/bash-completion/bash_completion ]; then
+  . "$(brew --prefix)"/share/bash-completion/bash_completion
+fi
+
 # Enable tab completion for `g` by marking it as an alias for `git`
 if type _git &> /dev/null && [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
 	complete -o default -o nospace -F _git g;
