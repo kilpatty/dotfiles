31e25ec27375f513427a9dfd93ce3aaeb205965e
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2017-03-03 10:02:59 -0600

    add automated minikube installation - check if this will auto update

---
+++ minikube/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#
+# Run all dotfiles installers.
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.17.1/minikube-darwin-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
