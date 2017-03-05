d79a2b14d492fe04e4b05b10589f1332147027fb
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2017-03-04 21:24:56 -0600

    move kubectl completion to completion.zsh

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,8 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+## Kubectl Completion
+if [ $commands[kubectl] ]; then
+  source <(kubectl completion zsh)
+fi
