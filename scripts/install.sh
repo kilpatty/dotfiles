41a1c951f095cd4442954248251fdc013882ced7
Sean Kilgarriff <seanpkilgarriff@gmail.com>
2017-04-10 08:33:11 -0500

    remove vim in favor of subtree, small edits to zsh and install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,3 +12,29 @@ brew bundle
 
 # find the installers and run them iteratively
 find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
+
+#!/usr/bin/env bash
+
+#this just installs Brew now, and then makes sure that the brew install bashed shell is defaulted.
+
+if ! which brew; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+fi;
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Run Homebrew through the Brewfile
+echo "› brew bundle"
+brew bundle
+
+
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
