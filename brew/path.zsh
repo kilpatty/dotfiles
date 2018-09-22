8896850271e6ff350f209c264b7753ee9e889777
s-pk <seanpkilgarriff@gmail.com>
2018-09-22 16:34:33 -0500

    remove old brew.sh file and add path variable to path.zsh with comment

--- brew/path.zsh
+++ brew/path.zsh
@@ -1,3 +1,5 @@
 #Make sure this path comes last............
 #
 #Probably manually update it in the zsh file that loads all paths.
+#XXX Not 100% if we want this first or last in the path variable. Let's make sure we get back to this.
+export PATH="/usr/local/bin:$PATH"
