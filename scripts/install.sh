ad2437d3048f95b5746a8aa355e16d9a574480c1
s-pk <seanpkilgarriff@gmail.com>
2018-09-22 16:31:25 -0500

    remove brew files and lines from install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,32 +6,8 @@ set -e
 
 cd "$(dirname $0)"/..
 
-# Run Homebrew through the Brewfile
-echo "› brew bundle"
-brew bundle
-
 # find the installers and run them iteratively
 find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
-
-#!/usr/bin/env bash
-
-#this just installs Brew now, and then makes sure that the brew install bashed shell is defaulted.
-
-if ! which brew; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-fi;
-
-# Make sure we’re using the latest Homebrew.
-brew update
-
-# Upgrade any already-installed formulae.
-brew upgrade
-
-# Run Homebrew through the Brewfile
-echo "› brew bundle"
-brew bundle
-
-
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
