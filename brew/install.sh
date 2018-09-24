aea244ff53fdc15a55a079cf7b6e5b17710c3fef
s-pk <seanpkilgarriff@gmail.com>
2018-09-24 10:34:05 -0500

    have brew run its own update script

--- brew/install.sh
+++ brew/install.sh
@@ -3,12 +3,8 @@ if ! which brew; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 fi;
 
-# Make sure we’re using the latest Homebrew.
-brew update
-
-# Upgrade any already-installed formulae.
-brew upgrade
+# Run Homebrew updating script
+sh ./update.sh
 
 # Run Homebrew through the Brewfile
-echo "› brew bundle"
 brew bundle
