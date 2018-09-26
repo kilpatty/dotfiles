13ffe68105899e4747b07227e9e65ef147f08767
s-pk <seanpkilgarriff@gmail.com>
2018-09-26 13:53:42 -0500

    remove kubectl and minikube

--- kubectl/completion.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-## Kubectl Completion
-if [ $commands[kubectl] ]; then
-  source <(kubectl completion zsh)
-fi
--- minikube/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Run all dotfiles installers.
-
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64
-chmod +x minikube
-sudo mv minikube /usr/local/bin/
