59400583b2c1760e9b5ffef3f814f3bf43e276a3
s-pk <seanpkilgarriff@gmail.com>
2018-11-25 13:11:04 -0600

    add check for git completions installed from Homebrew, and remove them.

--- brew/update.sh
+++ brew/update.sh
@@ -5,3 +5,12 @@ brew update
 # Upgrade any already-installed formulae.
 echo "Updating Brew Formulae..."
 brew upgrade
+
+
+# So it turns out that Homebrew disabled to the option to remove shitty git zsh completions.
+# This will check for the file, and remove it here - hopefully updates only occur using this file.
+# But, I don't want to check and delete file on every check, so this is the next best bet.
+
+[[ -f /usr/local/share/zsh/site-functions/_git ]] && \
+  rm  -f /usr/local/share/zsh/site-functions/_git
+
