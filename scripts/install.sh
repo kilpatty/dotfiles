f9c135aafac04351d7ebb2cc410f2dd6884ebece
Sean Kilgarriff <sean@Seans-MacBook-Pro.local>
2019-04-21 13:43:14 -0500

    remove bash shell for now

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,7 +10,8 @@ cd "$(dirname $0)"/..
 find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
+# Switch to ZSH TODO
+#if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+#  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+#  chsh -s /usr/local/bin/bash;
+#fi;
