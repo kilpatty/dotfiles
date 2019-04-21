46b41505f8dbfab6d75ed2e1cdc087126df120c8
Sean Kilgarriff <sean@Seans-MacBook-Pro.local>
2019-04-21 13:38:59 -0500

    add some comments to brew install script

--- brew/install.sh
+++ brew/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+echo "Checking for Homebrew install"
 if ! command -v brew; then
+    echo "Homebrew not found, installing now"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 else
     echo "Homebrew already installed!"
