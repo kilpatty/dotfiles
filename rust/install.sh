97c1ea4249ffdd7b97a54e1eb240426cf7b8e4b2
kilpatty <seanpkilgarriff@gmail.com>
2019-04-23 12:05:25 -0500

    add rustup components to install process

--- rust/install.sh
+++ rust/install.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+
 if ! command -v cargo; then
     echo "Installing Rust..."
     curl https://sh.rustup.rs -sSf | sh
+    rustup component add rls rust-analysis rust-src clippy rustfmt
 else
     echo "Rust already installed"
 fi;
