3fa41ef304e27236ae85c574459b96ce8005f7ed
kilpatty <seanpkilgarriff@gmail.com>
2021-11-10 12:06:19 -0600

    go: add golint to golang globals

--- go/install.sh
+++ go/install.sh
@@ -3,5 +3,6 @@
 #Keep all Go packages that we want globally installed here.
 go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -u golang.org/x/tools/cmd/gopls
+go get -u golang.org/x/lint/golint
 
 echo "Golang tools installed!"
