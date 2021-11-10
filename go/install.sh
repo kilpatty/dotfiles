980e74c46551409e9d1b497be66a4740500c94b4
kilpatty <seanpkilgarriff@gmail.com>
2021-11-10 12:13:08 -0600

    go: change from go get to new go install command. Remove gopls (handled by editor) and other lint

--- go/install.sh
+++ go/install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 #Keep all Go packages that we want globally installed here.
-go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
-go get -u golang.org/x/tools/cmd/gopls
-go get -u golang.org/x/lint/golint
+go install golang.org/x/lint/golint@latest
 
 echo "Golang tools installed!"
