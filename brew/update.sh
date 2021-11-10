da5200dab84c4c80579fc821e05db527714d7eb4
kilpatty <seanpkilgarriff@gmail.com>
2021-11-10 11:54:29 -0600

    brew: misc formatting

--- brew/update.sh
+++ brew/update.sh
@@ -6,7 +6,6 @@ brew update
 echo "Updating Homebrew Formulae..."
 brew upgrade
 
-
 # So it turns out that Homebrew disabled to the option to remove shitty git zsh completions.
 # This will check for the file, and remove it here - hopefully updates only occur using this file.
 # But, I don't want to check and delete file on every check, so this is the next best bet.
